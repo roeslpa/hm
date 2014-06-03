b4e297bcc292ca852dbdadbdf344731c94ec22e7
roeslpa <git@roeslpa.de>
2014-06-03 19:02:50 +0200

    added div to player.php
    Divs:
    <playerStatusContent></>
    <playerVariableContent>
    <playerTryForm></><playerHangmanImage></>
    <playerSolveForm></>
    <playerGameEndMessage></>
    </>

--- player.php
+++ player.php
@@ -11,31 +11,54 @@ if($_GET['try'] != '') {
 }
 //sel word from where gid=gid and leader=1
 
-$all_letters_guessed = print_word($word,$u_letters);
-$wrong_letters = get_wrong_letters($u_letters,$alphabet,1);
+echo "	<div class='playerStatusContent'>".$leaderName."<br>";
+$guessedAllLetters = print_word($word,$u_letters);
+$wrongLetters = get_wrong_letters($u_letters,$alphabet,1);
+echo "	</div>";
+
 if($gend == 1) {
-	echo "<br>wait until the leader starts the game..<br><a href='?s=g'>refresh</a>";
+	echo "	<br>wait until the leader starts the game..<br>
+			<a href='?s=g'>refresh</a>";
 } else {
-	if($wrong_letters < 10 && $u_word == '' && $all_letters_guessed != 1) {
-		echo "<form action='?s=g' method='get' autocomplete='off'><input type='hidden' name='s' value='g' style='display:inline-block; width: 100px;'>
-		<input type='text' name='try' size='2' autocomplete='off' autocorrect='off' class='field try-field' style='display:inline-block;'>
-		<input type='submit' value='try' style='display:inline-block; width: 70px;'></form>";
+	echo "<div class='playerVariableContent'>";
+	if($wrongLetters < 10 && $u_word == '' && $guessedAllLetters != 1) {
+		echo "	<div class='playerTryForm'>
+					<form action='?s=g' method='get' autocomplete='off'>
+						<input type='hidden' name='s' value='g' style='display:inline-block; width: 100px;'>
+						<input type='text' name='try' size='2' autocomplete='off' autocorrect='off' class='field try-field' style='display:inline-block;'>
+						<input type='submit' value='try' style='display:inline-block; width: 70px;'>
+					</form>
+				</div>";
 	
-	if($wrong_letters > '0') {
-		echo "<img src='images/".$wrong_letters.".gif' style='display:inline-block;'>";
-	}
-		echo "<form action='?s=g' method='get'><input type='hidden' name='s' value='g'>
-		<input type='text' name='solve' autocomplete='off' autocorrect='off'>
-		<input type='submit' value='solve'></form>";
-	} else if($wrong_letters > 9) {
-		echo "<br>you tried 10 wrong letters, wait until everyone finished the game and the leader submitted the points<br><a href='?s=g'>refresh</a>";
-		echo "<img src='images/10.gif' style='display:block;'>";
+		if($wrongLetters > '0') {
+			echo "<div class='playerHangmanImage'><img src='images/".$wrongLetters.".gif' style='display:inline-block;'></div>";
+		}
+		echo "	<div class='playerSolveForm'>
+					<form action='?s=g' method='get'>
+						<input type='hidden' name='s' value='g'>
+						<input type='text' name='solve' autocomplete='off' autocorrect='off'>
+						<input type='submit' value='solve'>
+					</form>
+				</div>";
+	} else if($wrongLetters > 9) {
+		echo "	<div class='playerGameEndMessage'>
+					<br>you tried 10 wrong letters, wait until everyone finished the game and the leader submitted the points<br>
+					<a href='?s=g'>refresh</a>
+					<img src='images/10.gif' style='display:block;'>
+				</div>";
 	} else if($u_word != '') {
-		echo "<br>you guessed <u>".$u_word."</u>, wait until everyone finished the game and the leader submitted the points<br><a href='?s=g'>refresh</a>";
-	} else if($all_letters_guessed == 1) {
+		echo "	<div class='playerGameEndMessage'>
+					<br>you guessed <u>".$u_word."</u>, wait until everyone finished the game and the leader submitted the points<br>
+					<a href='?s=g'>refresh</a>
+				</div>";
+	} else if($guessedAllLetters == 1) {
 		$query = mysql_query("update `hm` set `word` = '".$word."' where `id`='".$uid."'");
-		echo "<br>you guessed all letters right, wait until everyone finished the game and the leader submitted the points<br><a href='?s=g'>refresh</a>";
+		echo "	<div class='playerGameEndMessage'>
+					<br>you guessed all letters right, wait until everyone finished the game and the leader submitted the points<br>
+					<a href='?s=g'>refresh</a>
+				</div>";
 	}
+	echo "</div>";
 }
 
 ?>
