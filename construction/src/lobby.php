dfc38531cbf83c169534eba8f5bc10a7590b5495
roeslpa <git@roeslpa.de>
2014-06-05 15:32:52 +0200

    lobby fixed

--- construction/src/lobby.php
+++ construction/src/lobby.php
@@ -4,9 +4,9 @@
 	<?php
 		$query = mysql_query("select `gid`,`name` from `hm` where (`leader` = '1' and `gend` = '1')");
 		while($row = mysql_fetch_array($query))
-			echo "	<a href='?s=n&gid=".$row['gid']."'>".$row['name']."</a><br>";
+			echo "	<a href='?create=on&gameId=".$row['gid']."'>".$row['name']."</a><br>";
 	?>
-    <a href='?s=n'>New Game</a>
+    <a href='?create=on'>New Game</a>
     </div>
 
 </div>
